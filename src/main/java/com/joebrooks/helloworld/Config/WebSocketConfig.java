7137a9cc71afdbf7ede787773a7c85bc9c53c99a
joebrooks <lcw3176@naver.com>
2021-06-12 23:50:41 +0900

    소켓 설정

---
+++ src/main/java/com/joebrooks/helloworld/Config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.joebrooks.helloworld.Config;
+
+import com.joebrooks.helloworld.Handler.SocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final SocketHandler socketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(socketHandler, "/auth/socket");
+    }
+}
