b048023602206af5932933943bd7459cce9d0002
joebrooks <lcw3176@naver.com>
2021-06-17 16:04:48 +0900

    리팩토링

--- src/main/resources/static/js/Motion.js
+++ src/main/resources/static/js/Motion.js
@@ -1,51 +1,71 @@
-export class Motion{
+export class Motion {
 
-    constructor(id, webSocket) {
+    constructor(id, socketService) {
         this.id = id;
         this.target = document.getElementById(this.id);
-        this.webSocket = webSocket;
-        this.defalutVelocity = 1;
-        this.maxSpeed = 10;
-        this.position = 0;
+        this.socketService = socketService;
+        this.timeOut = 30;  // setInterval 시간
+
+        // y축 움직임 관련 변수(jump 기능)
+        this.defaultJump = 1;
+        this.jumpId = null;
+        this.maxJump = 10;
+        this.yPosition = 0;
+        this.isReadyToJumping = true;
+        //
+        
+        // x축 움직임 관련 변수
+        this.maxSpeed = 20;
+        this.velocityPlusValue = 1;
+        this.xPosition = 0;
         this.nowVelocity = 0;
-        this.stopVelocity = this.defalutVelocity;
-        this.endOfWindow = 1580;
-        this.startOfWindow = -100;
+        this.stopVelocity = this.velocityPlusValue;
         this.isGoingFront = false;
         this.isStopping = false;
-        this.stopId = null;
-        this.left = "L";
-        this.right = "R";
-        this.direction = null;
+        this.moveId = null;
+        //
+
+        // ui draw 관련 변수
+        this.characterTopRate = 90;
+        this.nameTagTopRate = 85;
+        this.conversationTopRate = 75;
+        this.endOfWindow = 1580;
+        this.startOfWindow = -100;
+        //
     }
 
     moveLeft(){
-        if(this.direction === this.right){
-            this.stopMove();
-            return;
-        }
-        this.direction = this.left;
-        this.isGoingFront = false;
+        if(this.moveId == null){
+            this.isGoingFront = false;
 
-        if(this.nowVelocity <= this.maxSpeed){
-            this.nowVelocity += this.defalutVelocity;
+            this.moveId = setInterval(this.xMoveProcess.bind(this), this.timeOut)
         }
 
-        this.draw();
+
     }
 
 
     moveRight(){
-        if(this.direction === this.left){
-            this.stopMove();
-            return;
+        if(this.moveId == null){
+            this.isGoingFront = true;
+
+            this.moveId = setInterval(this.xMoveProcess.bind(this), this.timeOut)
         }
+    }
 
-        this.direction = this.right;
-        this.isGoingFront = true;
-
-        if(this.nowVelocity <= this.maxSpeed){
-            this.nowVelocity += this.defalutVelocity;
+    xMoveProcess(){
+        if(this.isStopping){
+            if(this.nowVelocity > 0){
+                this.nowVelocity -= this.stopVelocity;
+            } else{
+                clearInterval(this.moveId);
+                this.isStopping = false;
+                this.moveId = null;
+            }
+        } else{
+            if(this.nowVelocity <= this.maxSpeed){
+                this.nowVelocity += this.velocityPlusValue;
+            }
         }
 
         this.draw();
@@ -53,63 +73,73 @@ export class Motion{
 
     stopMove(){
         if(!this.isStopping){
-            const timeOut = 30;
             this.isStopping = true;
-
-            this.stopId = setInterval(this.stopCallback.bind(this), timeOut);
         }
 
     }
 
-    stopCallback(){
-        if(this.nowVelocity > 0){
-            this.nowVelocity -= this.stopVelocity;
+
+    jump(){
+        if(this.jumpId == null){
+            this.jumpId = setInterval(this.yMoveProcess.bind(this), this.timeOut);
+        }
+    }
+
+    yMoveProcess(){
+        if(this.isReadyToJumping){
+            this.yPosition += this.defaultJump;
         } else{
-            this.isStopping = false;
-            this.direction = null;
-            clearInterval(this.stopId);
+            this.yPosition -= this.defaultJump;
         }
 
-        this.draw();
+        if(this.yPosition >= this.maxJump){
+            this.isReadyToJumping = false;
+        }
+
+        if(this.yPosition <= 0){
+            this.isReadyToJumping = true;
+            this.yPosition = 0;
+            clearInterval(this.jumpId);
+            this.jumpId = null;
+        }
+
+        if(this.moveId == null && !this.isStopping){
+            this.draw();
+        }
     }
 
     draw(){
-
-        if(this.position < this.startOfWindow){
-            this.position = this.endOfWindow;
+        if(this.xPosition < this.startOfWindow){
+            this.xPosition = this.endOfWindow;
         }
 
-        if(this.position > this.endOfWindow){
-            this.position = this.startOfWindow;
+        if(this.xPosition > this.endOfWindow){
+            this.xPosition = this.startOfWindow;
         }
 
 
         if(this.isGoingFront){
-            this.position += this.nowVelocity;
+            this.xPosition += this.nowVelocity;
         } else{
-            this.position -= this.nowVelocity;
+            this.xPosition -= this.nowVelocity;
         }
 
-
-        this.target.style.transform = "rotate(" + this.position + "deg)"
-        this.target.style.left = this.position + "px";
+        this.target.style.transform = "rotate(" + this.xPosition * 5 + "deg)"
+        this.target.style.top = this.characterTopRate - this.yPosition + "%";
+        this.target.style.left = this.xPosition + "px";
 
         let nameTag = document.getElementById(this.id + "tag");
-        nameTag.style.left = this.position + "px";
+        nameTag.style.left = this.xPosition + "px";
+        nameTag.style.top = this.nameTagTopRate - this.yPosition + "%";
 
         let conversation = document.getElementById(this.id + "conversation");
+
         if(conversation !== null){
-            conversation.style.left = this.position + "px";
+            conversation.style.left = this.xPosition + "px";
+            conversation.style.top = this.conversationTopRate - this.yPosition + "%";
         }
 
-
-        let data = {
-            command:2,
-            content:this.position,
-            id:this.id,
-        }
-
-        this.webSocket.send(JSON.stringify(data));
+        this.socketService.sendData(2, {x:this.xPosition, y:this.yPosition}, this.id);
     }
 
 }
