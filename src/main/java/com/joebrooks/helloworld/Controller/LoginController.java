80eae950f651c88142bab3f25781480c4650aaef
lcw3176 <lcw3176@naver.com>
2021-07-15 23:53:43 +0900

    에러 대응 추가

--- src/main/java/com/joebrooks/helloworld/Controller/LoginController.java
+++ src/main/java/com/joebrooks/helloworld/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.joebrooks.helloworld.Controller;
 
+import com.joebrooks.helloworld.Common.IpProvider;
 import com.joebrooks.helloworld.Common.JwtTokenProvider;
 import com.joebrooks.helloworld.Dto.User;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +28,7 @@ public class LoginController {
     private String cookieName;
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final IpProvider ipProvider;
 
     @GetMapping
     public String showForm(User user){
@@ -30,7 +37,19 @@ public class LoginController {
 
 
     @PostMapping
-    public String userLogin(HttpServletResponse res, Model model, User user){
+    public String userLogin(HttpServletRequest req, HttpServletResponse res, Model model, User user){
+
+        String ip = ipProvider.getIp(req);
+
+        if(User.userList.values().stream().anyMatch(i -> i.getIp().equals(ip))){
+            model.addAttribute("error", "existIp");
+            return "login";
+        }
+
+        if(User.userList.keySet().contains(user.getNickName())){
+            model.addAttribute("error", "existNickname");
+            return "login";
+        }
 
         if(user.getNickName() == null || user.getNickName().equals("")){
             model.addAttribute("error", "empty");
@@ -39,10 +58,12 @@ public class LoginController {
 
         Cookie jwtCookie = new Cookie(cookieName, jwtTokenProvider.createToken(user.getNickName()));
         jwtCookie.setMaxAge(60*60*3);
-
         res.addCookie(jwtCookie);
 
-        return "redirect:/auth/world";
+        user.setIp(ip);
+        User.userList.put(user.getNickName(), user);
+
+        return "world";
     }
 
 }
