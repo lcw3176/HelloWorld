879a008259b8ca2fe0b6051d78081c2c93ad4dc4
lcw3176 <lcw3176@naver.com>
2021-07-15 23:54:09 +0900

    유저 리스트 임시 추가

--- src/main/java/com/joebrooks/helloworld/Dto/User.java
+++ src/main/java/com/joebrooks/helloworld/Dto/User.java
@@ -2,8 +2,11 @@ package com.joebrooks.helloworld.Dto;
 
 import lombok.Data;
 
+import java.util.HashMap;
+
 @Data
 public class User {
+    public static HashMap<String, User> userList = new HashMap<>();
     private String nickName;
     private String ip;
 }
