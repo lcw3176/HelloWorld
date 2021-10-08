eb9227c31cdf55cb819e84a386eb1726e48461e2
joebrooks <lcw3176@naver.com>
2021-10-08 13:46:25 +0900

    메인 클래스

---
+++ src/test/java/com/joebrooks/mapshotlocalserver/MapshotLocalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joebrooks.mapshotlocalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapshotLocalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
