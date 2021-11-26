00276131a74da6c24a0473219715d0303649dc5f
joebrooks <lcw3176@naver.com>
2021-11-26 17:41:11 +0900

    프로젝트 변경

---
+++ src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
@@ -0,0 +1,13 @@
+package com.joebrooks.mapshotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapshotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapshotServerApplication.class, args);
+    }
+
+}
