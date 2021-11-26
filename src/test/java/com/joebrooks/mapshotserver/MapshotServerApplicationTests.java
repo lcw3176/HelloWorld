01c6f7c7d470d7bab57358a83800133ac7186e9c
joebrooks <lcw3176@naver.com>
2021-11-26 17:43:40 +0900

    테스트 코드

---
+++ src/test/java/com/joebrooks/mapshotserver/MapshotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joebrooks.mapshotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapshotServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
