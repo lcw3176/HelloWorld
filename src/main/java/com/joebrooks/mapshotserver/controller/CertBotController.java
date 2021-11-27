6ef81085d5c9f8c7904bda6b669ebaa4029ff22a
joebrooks <lcw3176@naver.com>
2021-11-27 12:01:53 +0900

    https

--- src/main/java/com/joebrooks/mapshotserver/controller/CertBotController.java
+++ src/main/java/com/joebrooks/mapshotserver/controller/CertBotController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/.well-known/acme-challenge")
+@RequestMapping("/.well-known/acme-challenge/4dzk68doGZKXGFZGbDnezMhDHpVd-rIK90JeiKB2CyY")
 public class CertBotController {
 
     @GetMapping
