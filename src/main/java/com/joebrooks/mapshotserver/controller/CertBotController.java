57787b95c5d4f1a75b7fa3e582f1f55f9514bbe9
joebrooks <lcw3176@naver.com>
2021-11-27 11:58:38 +0900

    https

---
+++ src/main/java/com/joebrooks/mapshotserver/controller/CertBotController.java
@@ -0,0 +1,16 @@
+package com.joebrooks.mapshotserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/.well-known/acme-challenge")
+public class CertBotController {
+
+    @GetMapping
+    public String value(){
+        return "4dzk68doGZKXGFZGbDnezMhDHpVd-rIK90JeiKB2CyY.gQsJ47naSi2zdq2RYb-lIdGLA7Laie_nXIV21JuqhMM";
+    }
+
+}
--- src/main/java/com/joebrooks/mapshotserver/controller/HomeController.java
+++ src/main/java/com/joebrooks/mapshotserver/controller/HomeController.java
@@ -3,14 +3,13 @@ package com.joebrooks.mapshotserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
     public String home(){
-        return "PeVJt7WP3zB7RS-Mc5YEo_DtFfADq41jXfxkfLhXkbk";
+        return "index";
     }
 }
