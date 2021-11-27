d0f8d05ba1333443f2d5dfb96b13363e12a1ec57
joebrooks <lcw3176@naver.com>
2021-11-27 11:31:51 +0900

    임시 코드

--- src/main/java/com/joebrooks/mapshotserver/controller/HomeController.java
+++ src/main/java/com/joebrooks/mapshotserver/controller/HomeController.java
@@ -3,13 +3,14 @@ package com.joebrooks.mapshotserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
     public String home(){
-        return "index";
+        return "PeVJt7WP3zB7RS-Mc5YEo_DtFfADq41jXfxkfLhXkbk";
     }
 }
