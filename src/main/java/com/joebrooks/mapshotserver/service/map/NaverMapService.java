d4f765b00ee6d9b28c2df7f2bd223c201bc5c669
joebrooks <lcw3176@naver.com>
2021-11-28 16:28:45 +0900

    작성중

--- src/main/java/com/joebrooks/mapshotserver/service/map/NaverMapService.java
+++ src/main/java/com/joebrooks/mapshotserver/service/map/NaverMapService.java
@@ -1,13 +1,35 @@
 package com.joebrooks.mapshotserver.service.map;
 
+import com.joebrooks.mapshotserver.dto.Coor;
+import com.joebrooks.mapshotserver.enumerate.CommonMapType;
+import com.joebrooks.mapshotserver.enumerate.MapProvider;
+import com.joebrooks.mapshotserver.enumerate.Radius;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NaverMapService {
+@AllArgsConstructor
+@NoArgsConstructor
+public class NaverMapService implements IMapService {
 
-    public byte[] getImage(){
+    private MapTypeSerivce mapTypeSerivce;
 
 
+    @Override
+    public byte[] getImage(int type, Radius radius, Coor coor){
+        String mapType = mapTypeSerivce.getTypeByProvider(MapProvider.naver, CommonMapType.values()[type]);
+//        map.setKey("네이버키값");
+
+
+        return null;
+    }
+
+    private Coor getFirstBlock(Coor centerCoor){
+        return null;
+    }
+
+    private Coor getNextCoor(){
         return null;
     }
 }
