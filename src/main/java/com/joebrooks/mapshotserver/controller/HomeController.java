db36591315d2166a5fb557f81ac53601e01bfa0a
joebrooks <lcw3176@naver.com>
2021-11-28 16:26:57 +0900

    service 추가

--- src/main/java/com/joebrooks/mapshotserver/controller/HomeController.java
+++ src/main/java/com/joebrooks/mapshotserver/controller/HomeController.java
@@ -1,15 +1,47 @@
 package com.joebrooks.mapshotserver.controller;
 
+import com.joebrooks.mapshotserver.dto.Map;
+import com.joebrooks.mapshotserver.enumerate.MapProvider;
+import com.joebrooks.mapshotserver.enumerate.UserRecvInfo;
+import com.joebrooks.mapshotserver.service.map.IMapService;
+import com.joebrooks.mapshotserver.service.map.KakaoMapService;
+import com.joebrooks.mapshotserver.service.map.NaverMapService;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
+@NoArgsConstructor
 public class HomeController {
+
+    private IMapService mapService;
+    private KakaoMapService kakaoMapService;
+    private NaverMapService naverMapService;
 
     @GetMapping
     public String home(){
         return "index";
+    }
+
+    @PostMapping
+    public ResponseEntity getImage(@RequestBody HashMap<String, Object> userMapInfo){
+
+        if(userMapInfo.get(UserRecvInfo.provider.toString()) == MapProvider.kakao){
+            mapService = kakaoMapService;
+            System.out.println("카카오");
+        } else {
+            mapService = naverMapService;
+            System.out.println("네이버");
+        }
+
+
+//        byte[] binary = mapService.getImage(type, radius, coor);
+
+//        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(binary);
+        return ResponseEntity.ok().body("헬로");
     }
 }
