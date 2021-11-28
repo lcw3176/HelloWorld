37cac4bd99645e2e06b3613b283069d782d8c3c7
joebrooks <lcw3176@naver.com>
2021-11-28 16:27:15 +0900

    Map 추가

---
+++ src/main/java/com/joebrooks/mapshotserver/dto/Map.java
@@ -0,0 +1,17 @@
+package com.joebrooks.mapshotserver.dto;
+
+import com.joebrooks.mapshotserver.enumerate.CommonMapType;
+import com.joebrooks.mapshotserver.enumerate.MapProvider;
+import com.joebrooks.mapshotserver.enumerate.Radius;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Map {
+    private MapProvider provider;
+    private CommonMapType mapType;
+    private Radius radius;
+    private Coor centerCoor;
+
+}
