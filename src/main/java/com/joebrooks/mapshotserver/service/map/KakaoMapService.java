f9533a6b3f77c071a224eb4db779dcade709fe6f
joebrooks <lcw3176@naver.com>
2021-11-28 16:28:26 +0900

    인터페이스 구현

--- src/main/java/com/joebrooks/mapshotserver/service/map/KakaoMapService.java
+++ src/main/java/com/joebrooks/mapshotserver/service/map/KakaoMapService.java
@@ -1,4 +1,14 @@
 package com.joebrooks.mapshotserver.service.map;
 
-public class KakaoMapService {
+import com.joebrooks.mapshotserver.dto.Coor;
+import com.joebrooks.mapshotserver.enumerate.Radius;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KakaoMapService implements IMapService {
+
+    @Override
+    public byte[] getImage(int type, Radius radius, Coor coor) {
+        return new byte[0];
+    }
 }
