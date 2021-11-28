31e8d75cf2f184700bc60030270d888abfe8d5cc
joebrooks <lcw3176@naver.com>
2021-11-28 16:26:13 +0900

    propertySource 추가

--- src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
+++ src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
@@ -2,8 +2,10 @@ package com.joebrooks.mapshotserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:radius.yml"})
 public class MapshotServerApplication {
 
     public static void main(String[] args) {
