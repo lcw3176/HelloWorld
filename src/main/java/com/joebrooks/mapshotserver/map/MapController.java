e7e6393d4e9f3b1e40ba4099e13f1d89a913ff35
joebrooks <lcw3176@naver.com>
2021-12-04 16:42:06 +0900

    맵 컨트롤러

---
+++ src/main/java/com/joebrooks/mapshotserver/map/MapController.java
@@ -0,0 +1,9 @@
+package com.joebrooks.mapshotserver.map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/map")
+public class MapController {
+}
