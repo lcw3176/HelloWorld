690025b25a493253424b93b59e9f3d473a2a78c5
joebrooks <lcw3176@naver.com>
2021-12-05 15:52:24 +0900

    익셉션 삭제

--- src/main/java/com/joebrooks/mapshotserver/user/service/UserLoggingService.java
+++ src/main/java/com/joebrooks/mapshotserver/user/service/UserLoggingService.java
@@ -1,13 +1,12 @@
 package com.joebrooks.mapshotserver.user.service;
 
-import com.joebrooks.mapshotserver.user.exception.InvalidUserException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserLoggingService {
 
     public void FirstVisit(){
-        throw new InvalidUserException("ssss");
+
     }
 
     public void MultipleVisit(){
