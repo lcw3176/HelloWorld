5021f508f5b7875e7e45a5dbc0c8be27d6c942ea
joebrooks <lcw3176@naver.com>
2021-12-05 15:51:22 +0900

    삭제

--- src/main/java/com/joebrooks/mapshotserver/user/exception/InvalidUserException.java
+++
@@ -1,8 +0,0 @@
-package com.joebrooks.mapshotserver.user.exception;
-
-public class InvalidUserException extends RuntimeException{
-
-    public InvalidUserException(String wrongValue){
-        super("User 잘못된 값 입력: " + wrongValue);
-    }
-}
