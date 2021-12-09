e318ea28a5b53a144e958ac4a4e79398903115d2
joebrooks <lcw3176@naver.com>
2021-12-09 17:54:09 +0900

    스케줄링 enable

--- src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
+++ src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
@@ -3,9 +3,11 @@ package com.joebrooks.mapshotserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class MapshotServerApplication {
 
     public static void main(String[] args) {
