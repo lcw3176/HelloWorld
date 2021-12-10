942a2f41f749ea5b15aaed18d8c162ece1139944
joebrooks <lcw3176@naver.com>
2021-12-11 00:34:10 +0900

    인코딩 추가

--- src/main/java/com/joebrooks/mapshotserver/service/SlackService.java
+++ src/main/java/com/joebrooks/mapshotserver/service/SlackService.java
@@ -1,8 +1,12 @@
 package com.joebrooks.mapshotserver.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class SlackService {
@@ -10,9 +14,10 @@ public class SlackService {
     @Value("${slack.url}")
     private String url;
 
-
-    public void sendMesseage(String message){
+    public void sendMessage(String message){
         RestTemplate rt = new RestTemplate();
+
+        rt.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
         rt.postForObject(url, message, String.class);
     }
 }
