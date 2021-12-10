0ac69d65848525c27ad87f34389b51698b189537
joebrooks <lcw3176@naver.com>
2021-12-11 00:34:51 +0900

    임시 테스트

---
+++ src/test/java/com/joebrooks/mapshotserver/service/JsonMessageServiceTest.java
@@ -0,0 +1,15 @@
+package com.joebrooks.mapshotserver.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class JsonMessageServiceTest {
+
+    private JsonMessageService jsonMessageService = new JsonMessageService();
+
+//    @Test
+//    public void Test(){
+//        jsonMessageService.TestMethod();
+//    }
+
+}
