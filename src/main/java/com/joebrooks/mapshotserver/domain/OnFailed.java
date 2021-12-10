233e64346a7e619662df43384e97dea8bf77eb5e
joebrooks <lcw3176@naver.com>
2021-12-11 00:33:46 +0900

    builder 제거

--- src/main/java/com/joebrooks/mapshotserver/domain/OnFailed.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/OnFailed.java
@@ -1,17 +1,13 @@
 package com.joebrooks.mapshotserver.domain;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-@Builder
 public class OnFailed {
     private String title;
     private String message;
-    private boolean firstVisit;
     private int usingCount;
-    private Date date;
     private String usedFunc;
 }
--- src/main/java/com/joebrooks/mapshotserver/domain/OnSuccess.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/OnSuccess.java
@@ -1,15 +1,11 @@
 package com.joebrooks.mapshotserver.domain;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-@Builder
 public class OnSuccess {
-    private boolean firstVisit;
     private int usingCount;
-    private Date date;
     private String usedFunc;
 }
