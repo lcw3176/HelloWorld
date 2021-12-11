ada7e2f8bb470f363e3b097bdabfd1d7f0856be4
joebrooks <lcw3176@naver.com>
2021-12-11 16:33:43 +0900

    url 환경변수 설정

--- src/main/java/com/joebrooks/mapshotserver/service/SlackService.java
+++ src/main/java/com/joebrooks/mapshotserver/service/SlackService.java
@@ -1,18 +1,19 @@
 package com.joebrooks.mapshotserver.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 @Service
 public class SlackService {
 
-    @Value("${slack.url}")
     private String url;
+
+    public SlackService(){
+        this.url = System.getenv("SLACK_URL");
+    }
 
     public void sendMessage(String message){
         RestTemplate rt = new RestTemplate();
