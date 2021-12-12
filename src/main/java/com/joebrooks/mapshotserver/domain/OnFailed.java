db5d00788a9731e98a5fa0ed8778fb52b5098f7f
joebrooks <lcw3176@naver.com>
2021-12-12 15:47:35 +0900

    import 제거

--- src/main/java/com/joebrooks/mapshotserver/domain/OnFailed.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/OnFailed.java
@@ -2,8 +2,6 @@ package com.joebrooks.mapshotserver.domain;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class OnFailed {
     private String title;
--- src/main/java/com/joebrooks/mapshotserver/domain/OnSuccess.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/OnSuccess.java
@@ -2,8 +2,6 @@ package com.joebrooks.mapshotserver.domain;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class OnSuccess {
     private int usingCount;
