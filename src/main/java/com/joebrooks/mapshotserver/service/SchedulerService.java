eab25c4ff2e1c87d53b5b241f9fd707f53d8503e
joebrooks <lcw3176@naver.com>
2021-12-14 17:06:19 +0900

    스케줄러 주석

---
+++ src/main/java/com/joebrooks/mapshotserver/service/SchedulerService.java
@@ -0,0 +1,18 @@
+package com.joebrooks.mapshotserver.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SchedulerService {
+
+//    private String myUrl = "https://mapshot-proxy.herokuapp.com";
+//    private String myUrl = "http://localhost:8080";
+//
+//    @Scheduled(cron = "0 */10 * * * *")
+//    public void preventSleepingHeroku(){
+//        RestTemplate rt = new RestTemplate();
+//        rt.getForEntity(myUrl, String.class);
+//    }
+}
