e8b3e0cc1d084a0e1c42346e28c4fc269b7b8b31
joebrooks <lcw3176@naver.com>
2021-12-17 00:32:13 +0900

    리턴 타입 변경

--- src/main/java/com/joebrooks/mapshotserver/service/ChromeDriverService.java
+++ src/main/java/com/joebrooks/mapshotserver/service/ChromeDriverService.java
@@ -4,8 +4,6 @@ import com.joebrooks.mapshotserver.domain.KakaoMap;
 import com.joebrooks.mapshotserver.util.ChromeDriverEx;
 import com.joebrooks.mapshotserver.util.ChromeOptionUtil;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
@@ -13,24 +11,19 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.PreDestroy;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class ChromeDriverService {
 
     private WebDriverWait waiter;
     private ChromeDriverEx driver;
-//    private static int waitNumber = 0;
 
     public ChromeDriverService() throws Exception {
         this.driver = new ChromeDriverEx(ChromeOptionUtil.getInstance().getOptions());
         this.waiter = new WebDriverWait(this.driver, 30);
     }
 
-    public synchronized String getImage(KakaoMap kakaoMapInfo) {
+    public synchronized byte[] getImage(KakaoMap kakaoMapInfo) {
         UriComponents uri = UriComponentsBuilder.newInstance()
                 .scheme("https")
                 .host("mapshot.herokuapp.com")
@@ -44,8 +37,7 @@ public class ChromeDriverService {
         driver.get(uri.toString());
         waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("checker_true")));
 
-
-        return driver.getFullScreenshotAs(OutputType.BASE64);
+        return driver.getFullScreenshot();
     }
 
     @PreDestroy
