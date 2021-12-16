d4f5091d34c18c7ea52bb95e84dc7a7e51c17024
joebrooks <lcw3176@naver.com>
2021-12-16 14:39:16 +0900

    코드 수정

--- src/main/java/com/joebrooks/mapshotserver/service/ChromeDriverService.java
+++ src/main/java/com/joebrooks/mapshotserver/service/ChromeDriverService.java
@@ -1,27 +1,35 @@
 package com.joebrooks.mapshotserver.service;
 
 import com.joebrooks.mapshotserver.domain.KakaoMap;
-import com.joebrooks.mapshotserver.util.CallableChromeDriver;
+import com.joebrooks.mapshotserver.util.ChromeDriverEx;
+import com.joebrooks.mapshotserver.util.ChromeOptionUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class ChromeDriverService {
 
-
-    private ExecutorService executorService;
-    private CallableChromeDriver callableChromeDriver;
+    private WebDriverWait waiter;
+    private ChromeDriverEx driver;
 
     public ChromeDriverService() throws Exception {
-        executorService = Executors.newFixedThreadPool(2);
-        callableChromeDriver = new CallableChromeDriver();
+        this.driver = new ChromeDriverEx(ChromeOptionUtil.getInstance().getOptions());
+        this.waiter = new WebDriverWait(this.driver, 30);
     }
 
-    public byte[] getImage(KakaoMap kakaoMapInfo) throws Exception {
+    public byte[] getImage(KakaoMap kakaoMapInfo) {
         UriComponents uri = UriComponentsBuilder.newInstance()
                 .scheme("https")
                 .host("mapshot.herokuapp.com")
@@ -32,9 +40,17 @@ public class ChromeDriverService {
                 .queryParam("type", kakaoMapInfo.getType())
                 .build(true);
 
-        callableChromeDriver.setKakaoMapInfo(uri);
+        driver.get(uri.toString());
+        waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("checker_true")));
+        byte[] srcFile = driver.getFullScreenshotAs(OutputType.BYTES);
 
-        return  executorService.submit(callableChromeDriver).get();
+
+        return srcFile;
+    }
+
+    @PreDestroy
+    private void closeDriver(){
+        this.driver.quit();
     }
 
 
