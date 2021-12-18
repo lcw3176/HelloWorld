1d3e16ba060e6ca0fc308616b3c2ee0642c6bb58
joebrooks <lcw3176@naver.com>
2021-12-18 15:51:10 +0900

    DailyReport 루틴

---
+++ src/main/java/com/joebrooks/mapshotserver/routine/DailyReport.java
@@ -0,0 +1,27 @@
+package com.joebrooks.mapshotserver.routine;
+
+import com.joebrooks.mapshotserver.domain.DailyInfo;
+import com.joebrooks.mapshotserver.service.MakeMessageService;
+import com.joebrooks.mapshotserver.service.SlackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DailyReport {
+
+    private final MakeMessageService makeDailyMessage;
+    private final SlackService slackService;
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    public void dailyReport(){
+        int todayUser = DailyInfo.getUserCount();
+        int todaySuccess = DailyInfo.getSuccessCount();
+        int todayFailed = DailyInfo.getFailedCount();
+
+        String stringifyJsonMessage = makeDailyMessage.makeDailyMessage(todayUser, todaySuccess, todayFailed);
+        slackService.sendMessage(stringifyJsonMessage);
+        DailyInfo.initData();
+    }
+}
