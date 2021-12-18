beb153ca17bbdb4103a8e4d85a8b2052c0d3844a
joebrooks <lcw3176@naver.com>
2021-12-18 15:50:28 +0900

    이름 변경

---
+++ src/main/java/com/joebrooks/mapshotserver/controller/KakaoMapController.java
@@ -0,0 +1,31 @@
+package com.joebrooks.mapshotserver.controller;
+
+import com.joebrooks.mapshotserver.domain.KakaoMap;
+import com.joebrooks.mapshotserver.service.ChromeDriverService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin("https://testservermapshot.netlify.app")
+@RestController
+@RequestMapping("/proxy/kakao")
+public class KakaoMapController {
+
+    private final ChromeDriverService chromeDriverService;
+
+    public KakaoMapController(ChromeDriverService chromeDriverService){
+        this.chromeDriverService = chromeDriverService;
+    }
+
+    @PostMapping
+    public ResponseEntity requestMapImage(@RequestBody KakaoMap kakaoMapInfo) {
+
+        byte[] srcFile = chromeDriverService.getImage(kakaoMapInfo);
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(srcFile);
+    }
+
+
+}
+
