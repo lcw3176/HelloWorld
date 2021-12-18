c07e00cb986ac4aeed3f3126de40a2ac51226a5e
joebrooks <lcw3176@naver.com>
2021-12-18 15:55:58 +0900

    설정 변경

--- src/main/java/com/joebrooks/mapshotserver/component/ChromeOptionEx.java
+++ src/main/java/com/joebrooks/mapshotserver/component/ChromeOptionEx.java
@@ -6,14 +6,14 @@ public class ChromeOptionEx {
 
     public ChromeOptions getOptions(){
 
-            System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-//            System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
+//            System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
             ChromeOptions options = new ChromeOptions();
-//            options.addArguments("--headless");
-//            options.addArguments("--no-sandbox");
-//            options.addArguments("--disable-gpu");
-//            options.addArguments("--disable-dev-shm-usage");
-//            options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-dev-shm-usage");
+            options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
 
         return options;
     }
