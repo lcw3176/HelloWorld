10e852cabcfd6c5b20a23ba9d44cc44eb92f6ff9
joebrooks <lcw3176@naver.com>
2022-01-01 23:32:43 +0900

    소켓 관련 추가

---
+++ src/main/java/com/joebrooks/mapshotserver/domain/kakaoMap/hadler/KakaoSocketHandler.java
@@ -0,0 +1,36 @@
+package com.joebrooks.mapshotserver.domain.kakaoMap.hadler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joebrooks.mapshotserver.domain.kakaoMap.dto.KakaoMap;
+import com.joebrooks.mapshotserver.domain.kakaoMap.service.ChromeDriverService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+@Component
+@RequiredArgsConstructor
+public class KakaoSocketHandler extends TextWebSocketHandler {
+
+    private final ChromeDriverService chromeDriverService;
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        KakaoMap kakaoMapInfo = mapper.readValue(message.getPayload(), KakaoMap.class);
+
+        if(!chromeDriverService.isRunning()){
+            byte[] src = chromeDriverService.getImage(kakaoMapInfo);
+            session.sendMessage(new BinaryMessage(src));
+            session.close();
+        } else {
+            session.sendMessage(new TextMessage("wait..."));
+        }
+    }
+
+
+}
