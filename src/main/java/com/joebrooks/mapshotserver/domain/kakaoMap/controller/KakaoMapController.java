3411fe35f2b55be60e42a676b18e0a5ef5405b3a
joebrooks <lcw3176@naver.com>
2022-01-01 23:32:23 +0900

    주석처리

--- src/main/java/com/joebrooks/mapshotserver/domain/kakaoMap/controller/KakaoMapController.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/kakaoMap/controller/KakaoMapController.java
@@ -2,30 +2,28 @@ package com.joebrooks.mapshotserver.domain.kakaoMap.controller;
 
 import com.joebrooks.mapshotserver.domain.kakaoMap.dto.KakaoMap;
 import com.joebrooks.mapshotserver.domain.kakaoMap.service.ChromeDriverService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin("https://testservermapshot.netlify.app")
-@RestController
-@RequestMapping("/proxy/kakao")
-public class KakaoMapController {
-
-    private final ChromeDriverService chromeDriverService;
-
-    public KakaoMapController(ChromeDriverService chromeDriverService){
-        this.chromeDriverService = chromeDriverService;
-    }
-
-    @PostMapping
-    public ResponseEntity requestMapImage(@RequestBody KakaoMap kakaoMapInfo) throws Exception {
-
-        byte[] srcFile = chromeDriverService.getImage(kakaoMapInfo);
-
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(srcFile);
-    }
-
-
-}
+//@CrossOrigin("https://testservermapshot.netlify.app")
+//@RestController
+//@RequestMapping("/proxy/kakao")
+//@RequiredArgsConstructor
+//public class KakaoMapController {
+//
+//    private final ChromeDriverService chromeDriverService;
+//
+//    @PostMapping
+//    public ResponseEntity requestMapImage(@RequestBody KakaoMap kakaoMapInfo) throws Exception {
+//
+//        byte[] srcFile = chromeDriverService.getImage(kakaoMapInfo);
+//
+//        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(srcFile);
+//    }
+//
+//
+//}
 
