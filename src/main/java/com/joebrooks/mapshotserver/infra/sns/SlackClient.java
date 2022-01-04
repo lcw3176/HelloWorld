4796d23ef2e3fb7a1a6718844e283f9c86015fe9
joebrooks <lcw3176@naver.com>
2022-01-04 14:36:08 +0900

    daliyReport 삭제

--- src/main/java/com/joebrooks/mapshotserver/infra/sns/SlackClient.java
+++ src/main/java/com/joebrooks/mapshotserver/infra/sns/SlackClient.java
@@ -1,12 +1,9 @@
 package com.joebrooks.mapshotserver.infra.sns;
 
-import com.joebrooks.mapshotserver.domain.userLogging.dto.DailyInfo;
-import com.joebrooks.mapshotserver.domain.userLogging.dto.OnFailed;
 import com.joebrooks.mapshotserver.infra.sns.config.SlackConfig;
-import com.joebrooks.mapshotserver.infra.sns.util.SlackMessageutil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,37 +12,16 @@ import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
-public class SlackClient {
+@Primary
+public class SlackClient implements ISnsClient {
 
-    private final SlackMessageutil messageUtil;
     private final SlackConfig slackConfig;
 
-
-    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
-    public void dailyReport(){
-        int todayUser = DailyInfo.getUserCount();
-        int todaySuccess = DailyInfo.getSuccessCount();
-        int todayFailed = DailyInfo.getFailedCount();
-
-        String stringifyJsonMessage = messageUtil.makeDailyMessage(todayUser, todaySuccess, todayFailed);
-
-        sendMessage(stringifyJsonMessage);
-
-        DailyInfo.initData();
-    }
-
-    public void sendFailedMessage(OnFailed failed){
-        String stringifyJsonMessage = messageUtil.makeFailedMessage(failed);
-
-        sendMessage(stringifyJsonMessage);
-    }
-
-
-    private void sendMessage(String message){
+    @Override
+    public void sendMessage(String message) {
         RestTemplate rt = new RestTemplate();
 
         rt.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
         rt.postForObject(slackConfig.getSlackUrl(), message, String.class);
     }
-
 }
