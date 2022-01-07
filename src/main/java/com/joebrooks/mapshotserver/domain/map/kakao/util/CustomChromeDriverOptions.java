aef794746f2335c6dd2ccdfc15fcdf0c0b93757d
joebrooks <lcw3176@naver.com>
2022-01-07 16:57:28 +0900

    드라이버 경로 변경

--- src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/util/CustomChromeDriverOptions.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/util/CustomChromeDriverOptions.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class CustomChromeDriverOptions {
 
     public ChromeOptions getOptions(){
-        System.setProperty("webdriver.chrome.driver", System.getenv("CHROMEDRIVER_PATH"));
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
-        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
+//        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
 
         return options;
     }
