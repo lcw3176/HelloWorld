eed0a4bcf8374f697fc05cdc695b105c90901bf8
joebrooks <lcw3176@naver.com>
2022-01-08 17:11:53 +0900

    대기 큐 추가

--- src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/controller/KakaoMapController.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/controller/KakaoMapController.java
@@ -7,6 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 @CrossOrigin("https://testservermapshot.netlify.app")
 @RestController
@@ -15,14 +20,24 @@ import org.springframework.web.bind.annotation.*;
 public class KakaoMapController {
 
     private final ChromeDriverService chromeDriverService;
+    private final Queue<HttpSession> waitQueue = new LinkedList<>();
+
 
     @GetMapping
-    public ResponseEntity getRequestAvailable() throws Exception {
-        if(chromeDriverService.isAvailable()){
+    public ResponseEntity getRequestAvailable(HttpServletRequest request) throws Exception {
+        HttpSession session = request.getSession();
+
+        if(!waitQueue.contains(session)){
+            waitQueue.add(session);
+        }
+
+        if(chromeDriverService.isAvailable() && waitQueue.peek().equals(session)){
+            waitQueue.poll();
+
             return ResponseEntity.ok().body(true);
         }
 
-        return ResponseEntity.ok().body(false);
+        return ResponseEntity.ok().body(waitQueue.size());
     }
 
     @PostMapping
