a30d813481c8f66f53d51c2c5780800efbeeb593
joebrooks <lcw3176@naver.com>
2022-01-08 15:55:50 +0900

    경로 변경

--- src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/util/CustomChromeDriverOptions.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/util/CustomChromeDriverOptions.java
@@ -2,16 +2,20 @@ package com.joebrooks.mapshotserver.domain.map.kakao.util;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class CustomChromeDriverOptions {
 
+    private final String chromeDriverPath = "CHROMEDRIVER_PATH";
+    private final String chromeBinPath = "GOOGLE_CHROME_BIN";
+
     public ChromeOptions getOptions(){
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", System.getenv(chromeDriverPath));
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
-//        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
+        options.setBinary(System.getenv(chromeBinPath));
 
         return options;
     }
