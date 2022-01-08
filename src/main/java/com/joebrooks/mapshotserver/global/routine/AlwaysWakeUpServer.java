7bcbba1993900efe3c5b469b4ae52e53274a2579
joebrooks <lcw3176@naver.com>
2022-01-08 21:25:14 +0900

    서버 sleep 방지 스케줄러

--- src/main/java/com/joebrooks/mapshotserver/global/routine/AlwaysWakeUpServer.java
+++ src/main/java/com/joebrooks/mapshotserver/global/routine/AlwaysWakeUpServer.java
@@ -1,16 +1,22 @@
 package com.joebrooks.mapshotserver.global.routine;
 
+import com.joebrooks.mapshotserver.global.common.UriInfo;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class AlwaysWakeUpServer {
 
-//    private String myUrl = "https://mapshot.herokuapp.com";
-//    private String myUrl = "http://localhost:8080";
-//
-//    @Scheduled(cron = "0 */10 * * * *")
-//    public void preventSleepingHeroku(){
-//        RestTemplate rt = new RestTemplate();
-//        rt.getForEntity(myUrl, String.class);
-//    }
+    private final String myUrl = UriComponentsBuilder.newInstance()
+            .scheme(UriInfo.scheme.toString())
+            .host(UriInfo.host.toString())
+            .build().toString();
+
+    @Scheduled(cron = "0 */10 * * * *")
+    public void preventSleepingHeroku(){
+        RestTemplate rt = new RestTemplate();
+        rt.getForEntity(myUrl, String.class);
+    }
 }
