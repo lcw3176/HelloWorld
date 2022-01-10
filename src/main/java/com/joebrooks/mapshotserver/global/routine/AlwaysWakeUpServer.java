f18f05c18bf0d953326886161676a9bc7cd32bf0
joebrooks <lcw3176@naver.com>
2022-01-10 21:43:45 +0900

    스케줄러 정지

--- src/main/java/com/joebrooks/mapshotserver/global/routine/AlwaysWakeUpServer.java
+++ src/main/java/com/joebrooks/mapshotserver/global/routine/AlwaysWakeUpServer.java
@@ -9,14 +9,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Component
 public class AlwaysWakeUpServer {
 
-    private final String myUrl = UriComponentsBuilder.newInstance()
-            .scheme(UriInfo.scheme.toString())
-            .host(UriInfo.host.toString())
-            .build().toString();
-
-    @Scheduled(cron = "0 */10 * * * *")
-    public void preventSleepingHeroku(){
-        RestTemplate rt = new RestTemplate();
-        rt.getForEntity(myUrl, String.class);
-    }
+//    private final String myUrl = UriComponentsBuilder.newInstance()
+//            .scheme(UriInfo.scheme.toString())
+//            .host(UriInfo.host.toString())
+//            .build().toString();
+//
+//    @Scheduled(cron = "0 */10 * * * *")
+//    public void preventSleepingHeroku(){
+//        RestTemplate rt = new RestTemplate();
+//        rt.getForEntity(myUrl, String.class);
+//    }
 }
