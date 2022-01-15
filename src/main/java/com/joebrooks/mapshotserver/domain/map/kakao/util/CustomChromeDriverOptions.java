d6170692c40f107d9fa34b061dffe12f83795626
joebrooks <lcw3176@naver.com>
2022-01-15 14:21:43 +0900

    옵션 Component화

--- src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/util/CustomChromeDriverOptions.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/util/CustomChromeDriverOptions.java
@@ -1,23 +1,27 @@
 package com.joebrooks.mapshotserver.domain.map.kakao.util;
 
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class CustomChromeDriverOptions {
 
     private final String chromeDriverPath = "CHROMEDRIVER_PATH";
     private final String chromeBinPath = "GOOGLE_CHROME_BIN";
+    private ChromeOptions options;
 
-    public ChromeOptions getOptions(){
+    public CustomChromeDriverOptions(){
         System.setProperty("webdriver.chrome.driver", System.getenv(chromeDriverPath));
-        ChromeOptions options = new ChromeOptions();
+        options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
         options.setBinary(System.getenv(chromeBinPath));
+    }
 
-        return options;
+    public ChromeOptions getOptions(){
+        return this.options;
     }
 
 }
