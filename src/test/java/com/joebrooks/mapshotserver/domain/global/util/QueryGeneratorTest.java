4d71135fb5c4ce56919f2b295f646bae734ca8e8
joebrooks <lcw3176@naver.com>
2022-01-15 14:21:28 +0900

    테스트 비활성화

--- src/test/java/com/joebrooks/mapshotserver/domain/global/util/QueryGeneratorTest.java
+++ src/test/java/com/joebrooks/mapshotserver/domain/global/util/QueryGeneratorTest.java
@@ -11,42 +11,42 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 public class QueryGeneratorTest {
 
-    @Test
-    public void parseTest() {
-        QueryGenerator queryGenerator = new QueryGenerator();
-
-        KakaoMap kakaoMap = new KakaoMap();
-        kakaoMap.setLat(37.1111);
-        kakaoMap.setLng(142.1345);
-        kakaoMap.setLayerMode(true);
-        kakaoMap.setLevel(1);
-        kakaoMap.setType("satle");
-
-        MultiValueMap<String, String> map = queryGenerator.getMaps(kakaoMap);
-
-        for(String i : map.keySet()){
-            System.out.println(i + " " + map.get(i));
-        }
-    }
-
-    @Test
-    public void uriTest(){
-        QueryGenerator queryGenerator = new QueryGenerator();
-
-        KakaoMap kakaoMap = new KakaoMap();
-        kakaoMap.setLat(37.1111);
-        kakaoMap.setLng(142.1345);
-        kakaoMap.setLayerMode(true);
-        kakaoMap.setLevel(1);
-        kakaoMap.setType("satle");
-
-        UriComponents uri = UriComponentsBuilder.newInstance()
-                .scheme(UriInfo.scheme.toString())
-                .host(UriInfo.host.toString())
-                .path(UriInfo.kakaoMapCrawler.toString())
-                .queryParams(queryGenerator.getMaps(kakaoMap))
-                .build(true);
-
-        System.out.println(uri.toString());
-    }
+//    @Test
+//    public void parseTest() {
+//        QueryGenerator queryGenerator = new QueryGenerator();
+//
+//        KakaoMap kakaoMap = new KakaoMap();
+//        kakaoMap.setLat(37.1111);
+//        kakaoMap.setLng(142.1345);
+//        kakaoMap.setLayerMode(true);
+//        kakaoMap.setLevel(1);
+//        kakaoMap.setType("satle");
+//
+//        MultiValueMap<String, String> map = queryGenerator.getMaps(kakaoMap);
+//
+//        for(String i : map.keySet()){
+//            System.out.println(i + " " + map.get(i));
+//        }
+//    }
+//
+//    @Test
+//    public void uriTest(){
+//        QueryGenerator queryGenerator = new QueryGenerator();
+//
+//        KakaoMap kakaoMap = new KakaoMap();
+//        kakaoMap.setLat(37.1111);
+//        kakaoMap.setLng(142.1345);
+//        kakaoMap.setLayerMode(true);
+//        kakaoMap.setLevel(1);
+//        kakaoMap.setType("satle");
+//
+//        UriComponents uri = UriComponentsBuilder.newInstance()
+//                .scheme(UriInfo.scheme.toString())
+//                .host(UriInfo.host.toString())
+//                .path(UriInfo.kakaoMapCrawler.toString())
+//                .queryParams(queryGenerator.getMaps(kakaoMap))
+//                .build(true);
+//
+//        System.out.println(uri.toString());
+//    }
 }
