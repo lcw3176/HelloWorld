89490bad1bb1c9c45c98aa16ac60ebc977e247ab
joebrooks <lcw3176@naver.com>
2022-01-21 18:40:03 +0900

    slackUrl 직접 정의, 구현 인터페이스 변경

--- src/main/java/com/joebrooks/mapshotserver/infra/sns/SlackClient.java
+++ src/main/java/com/joebrooks/mapshotserver/infra/sns/SlackClient.java
@@ -1,8 +1,8 @@
 package com.joebrooks.mapshotserver.infra.sns;
 
-import com.joebrooks.mapshotserver.infra.sns.config.SlackConfig;
+import com.joebrooks.mapshotserver.global.dto.ErrorMessage;
+import com.joebrooks.mapshotserver.infra.sns.util.SlackMessageUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -12,16 +12,19 @@ import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
-@Primary
-public class SlackClient implements ISnsClient {
+public class SlackClient implements IMessageClient{
 
-    private final SlackConfig slackConfig;
+    private final SlackMessageUtil slackMessageUtil;
+    private final String slackUrl = System.getenv("SLACK_URL");
 
     @Override
-    public void sendMessage(String message) {
+    public void sendMessage(ErrorMessage errors) {
         RestTemplate rt = new RestTemplate();
-
+        String message = slackMessageUtil.makeErrorMessage(errors);
         rt.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-        rt.postForObject(slackConfig.getSlackUrl(), message, String.class);
+        rt.postForObject(slackUrl, message, String.class);
     }
+
+
+
 }
