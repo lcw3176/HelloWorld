ef75a9c4067919b5afd1a058072a9e621b918664
joebrooks <lcw3176@naver.com>
2022-02-05 15:49:54 +0900

    스케줄링 제거, aop 추가

--- src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
+++ src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
@@ -2,11 +2,11 @@ package com.joebrooks.mapshotserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
-@EnableScheduling
+@EnableAspectJAutoProxy
 public class MapshotServerApplication {
 
     public static void main(String[] args) {
