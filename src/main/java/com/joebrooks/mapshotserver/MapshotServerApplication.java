269d5c10074e7fab74122cd3f1d6eece70bcf714
joebrooks <lcw3176@naver.com>
2022-02-06 00:15:52 +0900

    로깅 관련 수정

--- src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
+++ src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy
 public class MapshotServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/aspect/LogAspect.java
+++ src/main/java/com/joebrooks/mapshotserver/domain/map/kakao/aspect/LogAspect.java
@@ -2,46 +2,72 @@ package com.joebrooks.mapshotserver.domain.map.kakao.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
 import java.text.DecimalFormat;
 
-@Aspect
-@Component
-@Slf4j
-public class LogAspect {
-
-    @Around("execution(* com.joebrooks.mapshotserver.domain.map.kakao.service.*Service.*(..))")
-    public Object printMemoryLog(ProceedingJoinPoint joinPoint) throws Throwable{
-        String heapSize = byteCalculation(Long.toString(Runtime.getRuntime().totalMemory()));
-        String heapMaxSize = byteCalculation(Long.toString(Runtime.getRuntime().maxMemory()));
-        String heapFreeSize = byteCalculation(Long.toString(Runtime.getRuntime().freeMemory()));
-
-        String name = joinPoint.getSignature().getDeclaringTypeName();
-        log.debug(name + "." + joinPoint.getSignature().getName() + "()'");
-        log.debug("총 힙 사이즈: " + heapMaxSize  + "  현재 힙 사이즈: " + heapSize + "  잔여 힙 사이즈: " + heapFreeSize);
-
-        return joinPoint.proceed();
-    }
-
-    private String byteCalculation(String bytes) {
-        String retFormat = "0";
-        Double size = Double.parseDouble(bytes);
-
-        String[] s = {"bytes", "KB", "MB", "GB", "TB", "PB"};
-
-
-        if (bytes != "0") {
-            int idx = (int) Math.floor(Math.log(size) / Math.log(1024));
-            DecimalFormat df = new DecimalFormat("#,###.##");
-            double ret = ((size / Math.pow(1024, Math.floor(idx))));
-            retFormat = df.format(ret) + " " + s[idx];
-        } else {
-            retFormat += " " + s[0];
-        }
-
-        return retFormat;
-    }
-}
+//@Aspect
+//@Component
+//@Slf4j
+//public class LogAspect {
+//
+//    @Around("execution(* com.joebrooks.mapshotserver.domain.map.kakao.service.*Service.*(..))")
+//    public Object printMemoryLog(ProceedingJoinPoint joinPoint) throws Throwable{
+//        String name = joinPoint.getSignature().getDeclaringTypeName();
+//        String totalHeapSize = byteCalculation(getTotalHeapSize());
+//        String usedHeapSize = byteCalculation(getTotalHeapSize() - getFreeHeapSize());
+//
+//        log.debug(name + "." + joinPoint.getSignature().getName() + "()'");
+//        log.debug("총 힙 사이즈: " +  totalHeapSize);
+//        log.debug("사용중인 힙 사이즈: " + usedHeapSize);
+//        log.debug("메타스페이스 사이즈: " + byteCalculation(getMetaSpaceMemory()));
+//
+//        return joinPoint.proceed();
+//    }
+//
+//    private long getTotalHeapSize(){
+//        return Runtime.getRuntime().totalMemory();
+//    }
+//
+//    private long getFreeHeapSize(){
+//        return Runtime.getRuntime().freeMemory();
+//    }
+//
+//
+//    private long getMetaSpaceMemory(){
+//        for (MemoryPoolMXBean memoryMXBean : ManagementFactory.getMemoryPoolMXBeans()) {
+//            if ("Metaspace".equals(memoryMXBean.getName())) {
+//                return memoryMXBean.getUsage().getUsed();
+//            }
+//        }
+//
+//        return 0;
+//    }
+//
+//    private String byteCalculation(long bytes) {
+//        String retFormat = "0";
+//        Long size = bytes;
+//
+//        String[] s = {"bytes", "KB", "MB", "GB", "TB", "PB"};
+//
+//
+//        if (bytes != 0) {
+//            int idx = (int) Math.floor(Math.log(size) / Math.log(1024));
+//            DecimalFormat df = new DecimalFormat("#,###.##");
+//            double ret = ((size / Math.pow(1024, Math.floor(idx))));
+//            retFormat = df.format(ret) + " " + s[idx];
+//        } else {
+//            retFormat += " " + s[0];
+//        }
+//
+//        return retFormat;
+//    }
+//
+//
+//}
