a63088c8f0e55fe97b71eda8aef4ad37138cc7ef
joebrooks <lcw3176@naver.com>
2022-04-03 13:59:49 +0900

    ChromeDriverConfig

---
+++ src/main/java/com/joebrooks/mapshotserver/webDriver/ChromeDriverConfig.java
@@ -0,0 +1,41 @@
+package com.joebrooks.mapshotserver.webDriver;
+
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class ChromeDriverConfig {
+
+    @Bean
+    public ChromeOptions chromeOptions(){
+        String chromeDriverPath = "CHROMEDRIVER_PATH";
+        String chromeBinPath = "GOOGLE_CHROME_BIN";
+
+        System.setProperty("webdriver.chrome.driver", System.getenv(chromeDriverPath));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
+        options.setBinary(System.getenv(chromeBinPath));
+
+        return options;
+    }
+
+    @Bean(destroyMethod = "quit")
+    public CustomChromeDriver customChromeDriver() throws Exception {
+        return new CustomChromeDriver(chromeOptions());
+    }
+
+    @Bean
+    public WebDriverWait webDriverWait() throws Exception {
+        long timeOutSeconds = 30;
+
+        return new WebDriverWait(customChromeDriver(), Duration.ofSeconds(timeOutSeconds));
+    }
+}
