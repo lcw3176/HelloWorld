8f82c3ec22f2d1ecd289d2424818a066f79087a2
joebrooks <lcw3176@naver.com>
2022-04-03 13:16:21 +0900

    삭제

--- src/main/java/com/joebrooks/mapshotserver/infra/sns/IMessageClient.java
+++
@@ -1,7 +0,0 @@
-package com.joebrooks.mapshotserver.infra.sns;
-
-import com.joebrooks.mapshotserver.global.dto.ErrorMessage;
-
-public interface IMessageClient {
-    void sendMessage(ErrorMessage error);
-}
