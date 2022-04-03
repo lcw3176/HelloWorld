474dfe4918d634a5ccdd8735a01c84ef7b05cddc
joebrooks <lcw3176@naver.com>
2022-04-03 13:17:09 +0900

    주석 제거

--- src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
+++ src/main/java/com/joebrooks/mapshotserver/MapshotServerApplication.java
@@ -2,11 +2,9 @@ package com.joebrooks.mapshotserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
-//@EnableAspectJAutoProxy
 public class MapshotServerApplication {
 
     public static void main(String[] args) {
